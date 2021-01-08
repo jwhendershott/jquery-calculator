de34f9d4eb125d3e306aeea7d88ed7066714f30d
charliecan2 <carloscq920@gmail.com>
2021-01-08 13:16:54 -0600

    Latest changes to calculator. Need to actually make the equals work and attach an on click event listener to the clear button

--- logic.js
+++ logic.js
@@ -3,10 +3,13 @@ $(document).ready(function() {
     var firstNumber = $("#first-number");
     var secondNumber = $("#second-number");
     var operator = "";
+    var firstNumInt = parseInt(firstNumber.text());
+    var secNumInt = parseInt(secondNumber.text());
 
     $(".number").on("click", function() {
         if (operator == ""){
             firstNumber.append(this.value);
+            console.log(parseInt(firstNumber.text()));
         }
         else {
             secondNumber.append(this.value);
@@ -19,7 +22,10 @@ $(document).ready(function() {
 
     $(".equal").on("click", function (){
         //$("#result").text(this.children[0].innerHTML);
-        $("#result").append();
+
+        
+
+        //$("#result").(result);
     });
-    
+
 });
